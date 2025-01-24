7071a17a6aa11f7b015a034b407a25b64a36189a
MdArefineAhamedJoy <joy@nexstack.sg>
2025-01-25 01:19:31 +0600

    add border

--- src/components/ui/latestNews/LatestNews.tsx
+++ src/components/ui/latestNews/LatestNews.tsx
@@ -18,6 +18,56 @@ const storeData = [
     link: "/blog/new-year-strategies",
   },
 ];
+const latestNewses = [
+  {
+    image:
+      "https://s3-alpha-sig.figma.com/img/a4c6/c01e/8c2d27121baf738ca48d382161d2090c?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ce56A6IBvsiMlwvdr~Sd9obwAVSCo3~KDm8Ypng57iVU6AGzzBg2tAKQjrha2JtLuMV9v~MxJhs2JiUe~wfPTXX7R6TehF8VMMMDlbLWTo-LcC5hGhNkaTPowbl~RqK-dSv0VMQOurSmbn9Jcttd7RKgRAY6K459vu0dcK~ov7RtGUAZSb9lI3Sdhl0~GThfD4RbwbDpvTSN~6yLTZ7jFnx-~DuZqNHjS7E8CBs73aJW3XkjMxI6fpdK1Wa08n9c88Bdq1z0iVt07bFYfCZucK6I5t8WG0NxICp8B2rgwyOmVcr0O5P03AU7uFxBhu--sUSXj2LAw3bm-X4ut11OSg__",
+    title: "How to Attract Premium Customers with High-End Liquor Pricing",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+    link: "/blog/post-holiday-strategies",
+  },
+  {
+    image:
+      "https://s3-alpha-sig.figma.com/img/1b72/b430/359f13970d57316222b05bb951b69830?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=nGAiPee8grrsOzNRrsp64f9i2H43WJaTEsimgAv9A88YxwUKqT25F9hcsUXFCGIDqM9p2OHdxknWj5e82bKz62VugAwfjmogaqTvJtGOyadlaUicw9h9XWKdzx47cK8dLkoXTWL1YDFW3PLUvxWROm6naDT1~BCD5o6V7Yne7Ah3yoHSz7q6wAhwV-ez2pA7eku6cPG9skiZfhAvwss4AveV9valtI~ZMV0rfc-YD8WvnJtPmeboFtMJRRYJLoCrDcuULTLpUpwzX1C-YDyWJrRLlNY7P9fw1mCLoeU8nhWh0FeOMRXiLw2wMcNuyLIhqtpEQm6KIMrFP9vnUxkqGw__",
+    title: "How to Attract Premium Customers with High-End Liquor Pricing",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+    link: "/blog/new-year-strategies",
+  },
+  {
+    image:
+      "https://s3-alpha-sig.figma.com/img/a74e/ba62/37613ab21a5ddeaf210f0c50f6940770?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fVURUzADPB9XOfAQtuDJ0NR59Y-CEosQ~9EBUHsM73YKt4iRQx4M1enFG6a3diSqmdXt7L9HlY818ReJcGYl0KFq383UPxsRkK7bpIRbHnQrYqNc7Ws9j6OWI0Gln6Xngs6CU4tCUW8g7Faq2QUK0G2vW5iJPu-X95UHHAekyPiK8L2FdkFGTx0gOdrPrDrEK6cER70Ta~tOz3HCF7CoSitCZQsV4hNHuQs71o0CDASK3G0EUyYExXEybX1tXsGQoybC7yh0thnQ4b~SpcINztgg1TOdp4qRbmKthndYnyNDsMmHn5NFJEIoOEsl7NBr3Aw682W1kXcGRMP~xFEWFQ__",
+    title: "How to Attract Premium Customers with High-End Liquor Pricing",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+    link: "/blog/marketing-trends",
+  },
+  {
+    image:
+      "https://s3-alpha-sig.figma.com/img/f7f8/52ba/70c8fd9adde50c7ed9c90a4bb4dd8b1e?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Gq4Q9QRaknlq-NoFn2uxUZFe~ckjl-FWU2wANp3mUzy3MhHmJ66rnl7xFJOU8cy5IG1jYgqOMYEt2UNeNtglDZo~hi~gMUozKmp2E-tGD~dR28pIfd25JgP5aoNzaxrbc0eGXMd5CJcM76YvcMNUizKv3Zc5nfKke8lQjTVOXLOyTeOLqmfVE~PCgng2dIL7C3Mrtm0Atxvu3c~A17Z6ddZLWEYrkEfdlsU7R5jnfsJy8~0A6Y99gPiMhPYR0ViVRdA5j~K7blm1Ruv0g9AXjEZLK9VT~nAz92yew~5BWj0q1CNnPU7bxJeiPeUGSdK1bxmJoEKWTFSWNOy2TMeCHg__",
+    title: "How to Attract Premium Customers with High-End Liquor Pricing",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+    link: "/blog/marketing-trends",
+  },
+  {
+    image:
+      "https://s3-alpha-sig.figma.com/img/a74e/ba62/37613ab21a5ddeaf210f0c50f6940770?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fVURUzADPB9XOfAQtuDJ0NR59Y-CEosQ~9EBUHsM73YKt4iRQx4M1enFG6a3diSqmdXt7L9HlY818ReJcGYl0KFq383UPxsRkK7bpIRbHnQrYqNc7Ws9j6OWI0Gln6Xngs6CU4tCUW8g7Faq2QUK0G2vW5iJPu-X95UHHAekyPiK8L2FdkFGTx0gOdrPrDrEK6cER70Ta~tOz3HCF7CoSitCZQsV4hNHuQs71o0CDASK3G0EUyYExXEybX1tXsGQoybC7yh0thnQ4b~SpcINztgg1TOdp4qRbmKthndYnyNDsMmHn5NFJEIoOEsl7NBr3Aw682W1kXcGRMP~xFEWFQ__",
+    title: "How to Attract Premium Customers with High-End Liquor Pricing",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+    link: "/blog/marketing-trends",
+  },
+  {
+    image:
+      "https://s3-alpha-sig.figma.com/img/4cb3/b1e4/0b4f5a858f09677442a248beee45a092?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=G-SwLgu4wxnyK6Gh3ubktp~6ddmU6SChBfjtQqtO4sZbuF1Rta~XxwoWlLpRYyvb7obVgfqsJ9mooesw1dc36Y~00oh-eoE30EoQhDW-p9pNNOkWlQjNW5e0o5uoWybm0KZr82MaOaluXlRaDjgVtD~Xxxjc6-IE1LQ4r9frpNf5j-WjPHyN6~40obFsyHspBBxIkvGA-0CSuH3G-~8iJHPoNYJvVofp3C9EA9bRW8GlMrUcvhP2xdpKHz5r9cbD97WdCS3VBePdYee0n-I3GjqkXKrHEPZpjVVVQLLswZ0Gg~yEhk0Q5pwoee7NMlXw-oLzOrFx8PtIgty8st9~jQ__",
+    title: "How to Attract Premium Customers with High-End Liquor Pricing",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+    link: "/blog/marketing-trends",
+  },
+];
 const LatestNews = () => {
   return (
     <div className="mx-auto p-4 mb-16">
@@ -38,13 +88,14 @@ const LatestNews = () => {
               </div>
             </div>
           </section>
-          <div className="grid grid-cols-3">
-            <NewsCard />
-            <NewsCard />
-            <NewsCard />
-            <NewsCard />
-            <NewsCard />
-            <NewsCard />
+          <div className="grid grid-cols-3 gap-y-5">
+            {latestNewses.map((data, index) => (
+              <NewsCard
+                key={index}
+                data={data}
+                isLastInRow={(index + 1) % 3 === 0} 
+              />
+            ))}
           </div>
         </section>
         <section className="col-span-1">
--- src/components/ui/latestNews/NewsCard.tsx
+++ src/components/ui/latestNews/NewsCard.tsx
@@ -1,22 +1,24 @@
-const NewsCard = () => {
+import { TCardProps } from "@/types/card.type";
+
+const NewsCard = ({
+  data,
+  isLastInRow,
+}: {
+  data: TCardProps;
+  isLastInRow: boolean;
+}) => {
   return (
-    <div className="border border-red-500">
-      <div className=" height-[280px] pb-6 border-b border-red-400 ">
+    <div className={`${isLastInRow ? "" : "border-r border-gray-500"}`}>
+      <div className="h-[240px] pb-6 border-b border-gray-500">
         <img
           className="w-full h-full rounded px-6"
-          src="https://s3-alpha-sig.figma.com/img/c6e9/95b8/8660d5e6f184e2a86f043612a3221f47?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fAcxux0nFRVyVb-oDKKJYJsZB2lE~mp6U-Emq63ZE-x7EywXB6QEcVyF~lr5L0DH7yh65U~xYgSTTOqJHyXcGmup-xe8jSbee4y1JIpQUu8bg2lCq9pI6ddJyBq26bx3J9qSBM0HUSAtNstnJ-h3RRJx0RHp7pd88cr83Nek29MZr11KB1qlic5WIfp-bRPA3pbJrDZYk2lQoXvHey~yaPXQJW2hXRTQ-nNH5tH05gnfWW05aZL2TT~HhYtuNuVJat6Dpl7B3Ht6ginB-R7p-sUFiw~qdZWKQjmL6HouuNByvgAwLsCpOeag4a7mLUn4xipN32mUWajKzrsJKRqrmQ__"
-          alt=""
+          src={data.image}
+          alt={data.title}
         />
       </div>
       <div className="px-6 pb-6">
-        <h2 className="py-4 text-[22px] leading-[26.82px]">
-          How to Attract Premium Customers with High-End Liquor Pricing
-        </h2>
-        <p className="pb-4">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s,
-        </p>
+        <h2 className="py-4 text-[22px] leading-[26.82px]">{data.title}</h2>
+        <p className="pb-4">{data.description}</p>
         <a className="text-[15px] leading-[18.29px] font-bold" href="8">
           See All
         </a>
